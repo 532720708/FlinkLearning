9e74ed40e1f657c827093667f986c4f17913b685
downey <532720708@qq.com>
2020-01-02 14:33:59 +0800

    FlinkDemo

---
+++ DsInfoservice/src/main/java/cn/downey/dsinfoservice/DsInfoserviceApplication.java
@@ -0,0 +1,13 @@
+package cn.downey.dsinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsInfoserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsInfoserviceApplication.class, args);
+    }
+
+}
