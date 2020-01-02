74ea9a53ad3fc2dc1bf086cbd62f1f145fcafa52
downey <532720708@qq.com>
2020-01-02 15:07:46 +0800

    上报业务结束

--- DsInfoService/src/main/java/cn/downey/test/DsInfoCollectService.java
+++ DsInfoService/src/main/java/cn/downey/test/DsInfoCollectService.java
@@ -1,18 +1,49 @@
 package cn.downey.test;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 
 @Controller
 @RequestMapping("DsInfoCollectService")
 public class DsInfoCollectService {
 
-    @RequestMapping(value = "webInfoCollectService")
-    public void webInfoCollectService(String json, HttpServletRequest request, HttpServletResponse response){
+    @RequestMapping(value = "webInfoCollectService",method = RequestMethod.POST)
+    public void webInfoCollectService(@RequestBody String json, HttpServletRequest request, HttpServletResponse response){
         System.out.println("come in");
+        //业务开始
+
+        //业务结束
+        PrintWriter printWriter = getWriter(response);
+        printWriter.write("success");
+        closePrintWriter(printWriter);
+    }
+
+    private PrintWriter getWriter(HttpServletResponse response){
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("application/json");
+        OutputStream outputStream;
+        PrintWriter printWriter = null;
+        try{
+            outputStream = response.getOutputStream();
+            printWriter = new PrintWriter(outputStream);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return printWriter;
+
+    }
+
+    private void closePrintWriter(PrintWriter printWriter){
+        printWriter.flush();
+        printWriter.close();
+
     }
 }
